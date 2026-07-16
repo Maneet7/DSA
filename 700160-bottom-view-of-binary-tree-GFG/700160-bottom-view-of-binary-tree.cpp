46ef993fa357b1d496b615260fd6c9cf00309ff3
Maneet7 <149102365+Maneet7@users.noreply.github.com>
2026-07-16 14:31:17 +0530

    Added solution - Time: O(?), Space: O(?)

---
+++ 700160-bottom-view-of-binary-tree-GFG/700160-bottom-view-of-binary-tree.cpp
@@ -0,0 +1,43 @@
+/*
+Definition for Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+       vector<int>ans;
+        if(root==NULL)return ans;
+        queue<pair<Node*, int>>q;
+        
+        map<int, int> already;
+        q.push({root,0});
+        while(!q.empty()){
+            Node* node = q.front().first;
+            int hl = q.front().second;
+            q.pop();
+                
+            already[hl]=node->data;
+                
+            if(node->left) q.push({node->left, hl-1});
+            if(node->right) q.push({node->right, hl+1});
+        }
+        for(auto &i: already) ans.push_back(i.second);
+        return ans;
+
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
