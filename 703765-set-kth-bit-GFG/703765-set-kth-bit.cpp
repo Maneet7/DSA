32ec9d666f97741248bdefb6b275e10746cc2da4
Maneet7 <149102365+Maneet7@users.noreply.github.com>
2026-07-30 14:58:44 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703765-set-kth-bit-GFG/703765-set-kth-bit.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int setKthBit(int n, int k) {
+        return (1<<k|n);
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
