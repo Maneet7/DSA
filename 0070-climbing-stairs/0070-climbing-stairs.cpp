467e92a81066a1e4c1a3da1d1566008838f7eef7
Maneet7 <149102365+Maneet7@users.noreply.github.com>
2026-07-31 12:26:32 +0530

    Time: 0 ms (100%), Space: 8 MB (57.07%) - LeetHub - Time: O(n), Space: O(1)

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-vector<int>dp =vector<int>(46,-1);
     int climbStairs(int n) {
-        if(n<=2)return dp[n]= n;
-        if(dp[n]!= -1)return dp[n];
-        return dp[n]=climbStairs(n-1) + climbStairs(n-2);
+        vector<int>dp(46,-1);
+        dp[1]=1;
+        dp[0]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        } 
+        return dp[n];
     }
 };
 
