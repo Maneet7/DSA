e739c0e75e31d944c9a349d7a44ddf8f7d4175ed
Maneet7 <149102365+Maneet7@users.noreply.github.com>
2026-08-05 15:54:46 +0530

    Time: 27 ms (57.01%), Space: 27.4 MB (55.39%) - LeetHub - Time: O(?), Space: O(?)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-int lcsrec(string &s1, string &s2,int i,int j) {
-if(i==0 || j==0)return 0;
-if(dp[i][j]!=-1) return dp[i][j];
-if(s1[i-1]==s2[j-1]) return 1 +lcsrec(s1,s2,i-1,j-1);
-return dp[i][j]= max(lcsrec(s1,s2,i-1,j),lcsrec(s1,s2,i,j-1));
-}
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        dp.assign(n+1,vector<int>(m+1,-1));
-        return lcsrec(text1,text2,n,m);
-    }
-};
+// vector<vector<int>> dp;
+// int lcsrec(string &s1, string &s2,int i,int j) {
+// if(i==0 || j==0)return 0;
+// if(dp[i][j]!=-1) return dp[i][j];
+// if(s1[i-1]==s2[j-1]) return 1 +lcsrec(s1,s2,i-1,j-1);
+// return dp[i][j]= max(lcsrec(s1,s2,i-1,j),lcsrec(s1,s2,i,j-1));
+// }
+  int longestCommonSubsequence(string a, string b) {
+       int n=a.size();
+       int m=b.size();
+       vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(a[i-1]==b[j-1]){
+                dp[i][j]=1+ dp[i-1][j-1];
+            }
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+       }
+       return dp[n][m];
+   }
+
+ };
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
